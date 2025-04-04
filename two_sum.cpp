40fe9d241d3ab51a79806010ba5b42fa657bbb28
yashvidoshi <yashvi.doshi7@gmail.com>
2025-04-04 12:55:42 +0530

    Create two_sum.cpp

---
+++ two_sum.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+
+        unordered_map<int, int> m; 
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int diff = target - nums[i];
+
+            if (m.find(diff) != m.end())
+            {
+                return {m[diff], i};
+            }
+
+            m[nums[i]] = i;
+        }
+        return {};
+    }
+};
